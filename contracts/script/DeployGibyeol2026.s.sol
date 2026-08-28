ff9ad502efbbdf24168d22930cb37c64bb58a6c5
Yeonhan Jung <yt4307@gmail.com>
2026-08-28 22:31:42 +0900

    fix(contract): Base Sepolia 배포 안전장치
    - 대상 체인 ID와 복구 공개키 유효성 검증 추가

--- contracts/script/DeployGibyeol2026.s.sol
+++ contracts/script/DeployGibyeol2026.s.sol
@@ -12,13 +12,23 @@ interface Vm {
 contract DeployGibyeol2026 {
     Vm private constant VM = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
+    uint256 private constant TARGET_CHAIN_ID = 84_532;
     uint64 private constant UNLOCK_AT = 1_798_124_400;
     uint64 private constant UNLOCK_ROUND = 35_107_012;
     bytes32 private constant QUICKNET_CHAIN_HASH =
         0x52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971;
 
+    error WrongChainId(uint256 expected, uint256 actual);
+    error ZeroRecoveryPublicKey();
+
     function run() external returns (Gibyeol2026 deployed) {
+        if (block.chainid != TARGET_CHAIN_ID) {
+            revert WrongChainId(TARGET_CHAIN_ID, block.chainid);
+        }
+
         bytes32 recoveryPublicKey = VM.envBytes32("RECOVERY_PUBLIC_KEY");
+        if (recoveryPublicKey == bytes32(0)) revert ZeroRecoveryPublicKey();
+
         VM.startBroadcast();
         deployed = new Gibyeol2026(UNLOCK_AT, UNLOCK_ROUND, QUICKNET_CHAIN_HASH, recoveryPublicKey);
         VM.stopBroadcast();
